5a7cb3c55ac13dc8b870aacf13ee13e90c04ee99
wataru <wataru.corder@gmail.com>
2025-12-20 10:41:14 +0000

    feat: ログインページにユーザー名とパスワードの入力フォームを追加

--- src/features/auth/LoginPage.tsx
+++ src/features/auth/LoginPage.tsx
@@ -1,9 +1,35 @@
 'use client'
+import { useState } from "react";
 
 export default function LoginPage(){
+    const[username,setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('')
+const handleSubmit = () => {
+    //ログイン処理
+}
+
     return(
         <div>
             <h1>ログインページ</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                type="text"
+                value={username}
+                onChange={(e) => {
+                    setUsername(e.target.value)
+                }}
+                />
+                <input
+                type="text"
+                value={password}
+                onChange={(e) => {
+                    setPassword(e.target.value)
+                }}
+                />
+
+                <button type="submit">ログイン</button>
+                
+            </form>
         </div>
     )
 }
